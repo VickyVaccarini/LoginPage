d426d819aec647165af80d86e2a6051f198c2a07
Vicky Vaccarini <vicky_vaccarini@hotmail.com>
2025-11-18 00:45:22 -0300

    Correcciones

--- demo-project1-loginpage/src/main/java/com/example/demoproject1loginpage/config/CORSConfig.java
+++ demo-project1-loginpage/src/main/java/com/example/demoproject1loginpage/config/CORSConfig.java
@@ -9,7 +9,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:*", "https://*.azurewebsites.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- demo-project1-loginpage/src/main/java/com/example/demoproject1loginpage/controller/UserController.java
+++ demo-project1-loginpage/src/main/java/com/example/demoproject1loginpage/controller/UserController.java
@@ -3,13 +3,16 @@ package com.example.demoproject1loginpage.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.demoproject1loginpage.domain.User;
 import com.example.demoproject1loginpage.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
